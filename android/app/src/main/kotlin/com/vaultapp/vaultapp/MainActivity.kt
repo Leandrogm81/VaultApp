39baba7f7948b0e25a787b6071e4b5039e1e5687
Leandro Gobbo Menezes <leandrogmzl@gmail.com>
2026-06-10 09:21:54 -0300

    Sprint 0: Flutter project setup with Clean Architecture
    - Flutter 3.44.1, Dart 3.12.1
    - Clean Architecture: presentation/, domain/, data/, core/
    - Dependencies: drift, riverpod, go_router, cryptography, local_auth
    - CI: GitHub Actions (analyze + test)

---
+++ android/app/src/main/kotlin/com/vaultapp/vaultapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.vaultapp.vaultapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import cryptography_flutter
+import flutter_secure_storage_darwin
+import local_auth_darwin
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  CryptographyFlutterPlugin.register(with: registry.registrar(forPlugin: "CryptographyFlutterPlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
+}
